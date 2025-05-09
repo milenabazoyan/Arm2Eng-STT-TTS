f68317326b9d328c852bcdb4624e70aae5a1c24e
milenabazoyan <92885934+milenabazoyan@users.noreply.github.com>
2025-05-09 23:16:39 +0400

    Update translation_service.py

--- api/services/translation_service.py
+++ api/services/translation_service.py
@@ -1,15 +1,15 @@
-#import boto3
+import boto3
 
-#amazon_translate_client = boto3.client("translate", region_name="us-east-1")
+amazon_translate_client = boto3.client("translate", region_name="us-east-1")
 
 def translate(text):
-    return "And it was not known to anyone else."
-#    response = amazon_translate_client.translate_text(
-#        Text = text,
-#        SourceLanguageCode = "hy",
-#        TargetLanguageCode = "en"
-#    )
-#    translated_text = response['TranslatedText']
-#    print(f"Armenian text: {text}")
-#    print(f"Translated English text: {translated_text}")
-#    return translated_text
+#    return "And it was not known to anyone else."
+    response = amazon_translate_client.translate_text(
+        Text = text,
+        SourceLanguageCode = "hy",
+        TargetLanguageCode = "en"
+    )
+    translated_text = response['TranslatedText']
+    print(f"Armenian text: {text}")
+    print(f"Translated English text: {translated_text}")
+    return translated_text
